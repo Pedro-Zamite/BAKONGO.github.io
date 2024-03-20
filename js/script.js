f8bc3dc57e642805734ef5577465035c985b51f9
Pedro-Zamite <pedrozamite1@gmail.com>
2024-03-20 18:28:31 +0100

    alterando

--- js/script.js
+++ js/script.js
@@ -1,5 +1,54 @@
-import {btn_close, btn_contratar, btn_contratar2, btn_contratar3, contato, menu_itens, btn_paleta, modal_Paleta, hamburgue, menu, spread, spread2, spread3, spread4, spread5, spread6, modal, modal2, modal3, EuQuero, EuQuero2, EuQuero3, body} from "./_variaveis.js";
-import {radio} from "./_slideMobile.js";
+
+const contato=document.querySelector("#contato")
+const menu_itens=document.querySelector("#menu-itens")
+
+
+const btn_close=document.querySelector("#btn-close")
+const btn_paleta=document.querySelector(".btn-paleta")
+const modal_Paleta=document.querySelector(".modal-paleta")
+const btn_contratar=document.querySelector(".btn_contratar")
+const btn_contratar2=document.querySelector(".btn_contratar2")
+const btn_contratar3=document.querySelector(".btn_contratar3")
+
+
+const spread=document.querySelector(".spread")
+const spread2=document.querySelector("#spread2");
+const spread3=document.querySelector("#spread3");
+const spread4=document.querySelector(".spread4")
+const spread5=document.querySelector(".spread5")
+const spread6=document.querySelector(".spread6")
+
+const modal=document.querySelector(".modal");
+const modal2=document.querySelector(".modal2");
+const modal3=document.querySelector(".modal3");
+
+const EuQuero=document.querySelector(".EuQuero");
+const EuQuero2=document.querySelector(".EuQuero2");
+const EuQuero3=document.querySelector(".EuQuero3");
+
+const body=document.querySelector("body");
+
+// Inicio da formatação do menu hamburgue
+const hamburgue=document.querySelector("#hamburgue");
+const menu=document.querySelector("#menu");
+
+
+
+
+//?    Aqui começa a formatação do slide Mobile
+var radio=document.querySelector('.btn_manual');
+document.getElementById('radio1').checked=true;
+var cont=1;
+ setInterval(()=>{
+   proximoCard()
+},7000);
+function proximoCard(){
+   cont++;
+   if(cont > 3){
+       cont=1;
+   }
+   document.getElementById('radio'+cont).checked=true;
+}
 
 //! Inicio da Formatação do slide header
 var radial=document.querySelector('.manual-btn');
